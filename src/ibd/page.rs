42d8ca0399851f3f58263b4603b73dc93cdb82c7
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-07-22 20:57:07 +0800

    add: 添加 Index 页面解析

--- src/ibd/page.rs
+++ src/ibd/page.rs
@@ -500,21 +500,72 @@ impl INodeEntry {
 }
 
 // Index Page
-#[derive(Debug)]
 pub struct IndexPage {
-    index_header: IndexHeader,
-    fseg_header: FSegHeader,
-    infimum: GenericRow,
-    supremum: GenericRow,
+    /// Index Header
+    index_header: IndexHeader, // Index Header
+    /// FSEG Header
+    fseg_header: FSegHeader, // FSEG Header
+    /// System Record
+    infimum: Record, // infimum_extra[], see page0page.h
+    supremum: Record, // supremum_extra_data[], see page0page.h
+    /// User Records, grow down
+
+    ////////////////////////////////////////
+    //  Free Space
+    ////////////////////////////////////////
+
+    /// Page Directory, grows up
+    dir_slots: Vec<u16>, // page directory slots
+}
+
+impl fmt::Debug for IndexPage {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("IndexPage")
+            .field("index_header", &self.index_header)
+            .field("fseg_header", &self.fseg_header)
+            .field("infimum", &self.infimum)
+            .field("supremum", &self.supremum)
+            .field("dir_slots", &format!("{:?}", &self.dir_slots))
+            .finish()
+    }
 }
 
 impl BasePageOperation for IndexPage {
     fn new(buffer: Bytes) -> Self {
+        let idx_hdr = IndexHeader::new(buffer.slice(0..36));
+        let n_slots = idx_hdr.page_n_dir_slots as usize;
+        let mut slots = vec![0; n_slots];
+        for (offset, element) in slots.iter_mut().enumerate() {
+            let end = buffer.len() - offset * 2;
+            let beg = end - 2;
+            *element = u16::from_be_bytes(buffer.as_ref()[beg..end].try_into().unwrap());
+        }
         Self {
-            index_header: IndexHeader::new(buffer.slice(0..36)),
+            index_header: idx_hdr,
             fseg_header: FSegHeader::new(buffer.slice(36..56)),
-            infimum: GenericRow::new(buffer.slice(56..69)),
-            supremum: GenericRow::new(buffer.slice(69..82)),
+            infimum: Record::new(buffer.slice(56..69)),
+            supremum: Record::new(buffer.slice(69..82)),
+            dir_slots: slots,
+        }
+    }
+}
+
+#[repr(u8)]
+#[allow(non_camel_case_types)]
+#[allow(clippy::upper_case_acronyms)]
+#[derive(Debug, EnumDisplay, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub enum PageFormat {
+    REDUNDANT = 0,
+    COMPACT = 1,
+    MARKED(u8),
+}
+
+impl From<u8> for PageFormat {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => PageFormat::REDUNDANT,
+            1 => PageFormat::COMPACT,
+            _ => PageFormat::MARKED(value),
         }
     }
 }
@@ -528,6 +579,7 @@ pub struct IndexHeader {
     page_heap_top: u16,
     /// number of records in the heap, bit 15=flag: new-style compact page
     /// format
+    page_format: PageFormat,
     page_n_heap: u16,
     /// pointer to start of page free record list
     page_free: u16,
@@ -556,10 +608,13 @@ pub struct IndexHeader {
 
 impl IndexHeader {
     pub fn new(buffer: Bytes) -> Self {
+        let n_heap = u16::from_be_bytes(buffer.as_ref()[4..6].try_into().unwrap());
+        let fmt_flag = ((n_heap & 0x8000) >> 15) as u8;
         Self {
             page_n_dir_slots: u16::from_be_bytes(buffer.as_ref()[..2].try_into().unwrap()),
             page_heap_top: u16::from_be_bytes(buffer.as_ref()[2..4].try_into().unwrap()),
-            page_n_heap: u16::from_be_bytes(buffer.as_ref()[4..6].try_into().unwrap()),
+            page_format: fmt_flag.into(),
+            page_n_heap: n_heap,
             page_free: u16::from_be_bytes(buffer.as_ref()[6..8].try_into().unwrap()),
             page_garbage: u16::from_be_bytes(buffer.as_ref()[8..10].try_into().unwrap()),
             page_last_insert: u16::from_be_bytes(buffer.as_ref()[10..12].try_into().unwrap()),
@@ -599,19 +654,86 @@ impl FSegHeader {
     }
 }
 
-#[derive(Debug)]
-pub struct GenericRow {
-    row_flags: Bytes,
-    page_offset: u16,
-    record: Bytes,
+#[repr(u8)]
+#[allow(non_camel_case_types)]
+#[allow(clippy::upper_case_acronyms)]
+#[derive(Debug, EnumDisplay, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub enum RecordStatus {
+    REC_STATUS_ORDINARY = 0,
+    REC_STATUS_NODE_PTR = 1,
+    REC_STATUS_INFIMUM = 2,
+    REC_STATUS_SUPREMUM = 3,
+    MARKED(u8),
 }
 
-impl GenericRow {
+impl From<u8> for RecordStatus {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => RecordStatus::REC_STATUS_ORDINARY,
+            1 => RecordStatus::REC_STATUS_NODE_PTR,
+            2 => RecordStatus::REC_STATUS_INFIMUM,
+            3 => RecordStatus::REC_STATUS_SUPREMUM,
+            _ => RecordStatus::MARKED(value),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Record {
+    rec_hdr: RecordHeader, // record header
+    row_data: Bytes,       // row data
+}
+
+impl Record {
     pub fn new(buffer: Bytes) -> Self {
         Self {
-            row_flags: buffer.slice(..3),
-            page_offset: u16::from_be_bytes(buffer.as_ref()[3..5].try_into().unwrap()),
-            record: buffer.slice(5..),
+            rec_hdr: RecordHeader::new(buffer.slice(..5)),
+            row_data: buffer.slice(5..),
         }
+    }
+}
+
+#[derive(Debug)]
+pub struct RecordHeader {
+    info_bits: u8,            // 4 bits, MIN_REC/DELETED/VERSION/INSTANT, see rec.h
+    n_owned: u8,              // 4 bits
+    heap_no: u16,             // 13 bits
+    rec_status: RecordStatus, // 3 bits, see rec.h
+    next_rec_offset: u16,     // next record offset
+}
+
+impl RecordHeader {
+    pub fn new(buffer: Bytes) -> Self {
+        let b1 = u16::from_be_bytes(buffer.as_ref()[1..3].try_into().unwrap());
+        let status = (b1 & 0x0007) as u8;
+        Self {
+            info_bits: (buffer[0] & 0xf0) >> 4,
+            n_owned: (buffer[0] & 0x0f),
+            heap_no: (b1 & 0xfff8) >> 3,
+            rec_status: status.into(),
+            next_rec_offset: u16::from_be_bytes(buffer.as_ref()[3..5].try_into().unwrap()),
+        }
+    }
+
+    // Info bit denoting the predefined minimum record: this bit is set if and
+    // only if the record is the first user record on a non-leaf B-tree page
+    // that is the leftmost page on its level (PAGE_LEVEL is nonzero and
+    // FIL_PAGE_PREV is FIL_NULL).
+    const REC_INFO_MIN_REC_FLAG: u8 = 1;
+    // The deleted flag in info bits; when bit is set to 1, it means the record
+    // has been delete marked
+    const REC_INFO_DELETED_FLAG: u8 = 2;
+    // Use this bit to indicate record has version
+    const REC_INFO_VERSION_FLAG: u8 = 4;
+    // The instant ADD COLUMN flag. When it is set to 1, it means this record
+    // was inserted/updated after an instant ADD COLUMN.
+    const REC_INFO_INSTANT_FLAG: u8 = 8;
+
+    pub fn is_min_rec(&self) -> bool {
+        (self.info_bits & Self::REC_INFO_MIN_REC_FLAG) > 0
+    }
+
+    pub fn is_deleted(&self) -> bool {
+        (self.info_bits & Self::REC_INFO_DELETED_FLAG) > 0
     }
 }
