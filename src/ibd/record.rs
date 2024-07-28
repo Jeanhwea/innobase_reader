d510b3e59629d25135cd708dc6e84a7954cd073d
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-07-28 20:40:18 +0800

    fix: 调试变长字段长度计算方式

--- src/ibd/record.rs
+++ src/ibd/record.rs
@@ -137,12 +137,12 @@ impl RowInfo {
             2 => {
                 let b0 = self.vfld_arr[off + 1] as usize;
                 let b1 = self.vfld_arr[off] as usize;
-                let vlen = if b0 > REC_N_FIELDS_ONE_BYTE_MAX {
+                let vlen = if b0 > REC_N_FIELDS_ONE_BYTE_MAX.into() {
                     b0 + ((b1 & (REC_N_FIELDS_ONE_BYTE_MAX as usize)) << 8)
                 } else {
-                    b0
+                    b1
                 };
-                debug!("{:02x} {:02x} => {}", b0, b1, vlen);
+                debug!("b0=0x{:02x}, b1=0x{:02x} => {}", b0, b1, vlen);
                 vlen
             }
             _ => 0,
