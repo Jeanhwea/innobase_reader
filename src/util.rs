4a8bd2fa7acc80693946c2aeed848a483afbebaf
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-07-29 15:44:33 +0800

    update

--- src/util.rs
+++ src/util.rs
@@ -60,7 +60,7 @@ pub fn fmt_addr<T>(d: &T, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::E
 where
     T: Display + LowerHex,
 {
-    write!(f, "@0x{:x}({})", d, d.to_string().blue())
+    write!(f, "0x{:04x}@({})", d, d.to_string().blue())
 }
 
 pub fn zlib_uncomp(input: Bytes) -> Result<String> {
