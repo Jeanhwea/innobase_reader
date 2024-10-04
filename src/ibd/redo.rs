f32150a68d79ec3ccd40377217686859bd9458f0
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-10-04 21:02:54 +0800

    add: 添加解析 RedoLogRecordType 的类型

--- src/ibd/redo.rs
+++ src/ibd/redo.rs
@@ -2,6 +2,9 @@ use std::sync::Arc;
 
 use bytes::Bytes;
 use derivative::Derivative;
+use num_enum::FromPrimitive;
+use serde_repr::{Deserialize_repr, Serialize_repr};
+use strum::{Display, EnumString};
 
 use crate::util;
 
@@ -233,6 +236,9 @@ pub struct LogBlock {
     /// (4 bytes) last checksum
     #[derivative(Debug(format_with = "util::fmt_hex32"))]
     pub checksum: u32,
+
+    /// log record
+    pub record: Option<LogRecord>,
 }
 
 impl LogBlock {
@@ -242,12 +248,24 @@ impl LogBlock {
 
     pub fn new(addr: usize, buf: Arc<Bytes>) -> Self {
         let b0 = util::u32_val(&buf, addr);
+
+        let first_rec_offset = util::u16_val(&buf, addr + 6);
+        let rec = if first_rec_offset > 0 {
+            Some(LogRecord::new(
+                addr + (first_rec_offset as usize),
+                buf.clone(),
+            ))
+        } else {
+            None
+        };
+
         Self {
             hdr_no: b0 & (!Self::LOG_BLOCK_FLUSH_BIT_MASK),
             flush_flag: b0 & Self::LOG_BLOCK_FLUSH_BIT_MASK > 0,
             data_len: util::u16_val(&buf, addr + 4),
-            first_rec_group: util::u16_val(&buf, addr + 6),
+            first_rec_group: first_rec_offset,
             epoch_no: util::u32_val(&buf, addr + 8),
+            record: rec,
             checksum: util::u32_val(&buf, addr + OS_FILE_LOG_BLOCK_SIZE - 4),
             buf: buf.clone(),
             addr,
@@ -266,11 +284,257 @@ pub struct LogRecord {
     /// block data buffer
     #[derivative(Debug = "ignore")]
     pub buf: Arc<Bytes>,
+
+    /// log record header
+    pub log_rec_hdr: LogRecordHeader,
 }
 
 impl LogRecord {
     pub fn new(addr: usize, buf: Arc<Bytes>) -> Self {
         Self {
+            log_rec_hdr: LogRecordHeader::new(addr, buf.clone()),
+            buf: buf.clone(),
+            addr,
+        }
+    }
+}
+
+/// types of a redo log record
+#[repr(u8)]
+#[derive(Debug, Display, Default, Eq, PartialEq, Ord, PartialOrd, Clone)]
+#[derive(Deserialize_repr, Serialize_repr, EnumString, FromPrimitive)]
+pub enum LogRecordTypes {
+    /// if the mtr contains only one log record for one page, i.e.,
+    /// write_initial_log_record has been called only once, this flag is ORed to
+    /// the type of that first log record
+    MLOG_SINGLE_REC_FLAG = 128,
+
+    /// one byte is written
+    MLOG_1BYTE = 1,
+
+    /// 2 bytes ...
+    MLOG_2BYTES = 2,
+
+    /// 4 bytes ...
+    MLOG_4BYTES = 4,
+
+    /// 8 bytes ...
+    MLOG_8BYTES = 8,
+
+    /// Record insert
+    MLOG_REC_INSERT_8027 = 9,
+
+    /// Mark clustered index record deleted
+    MLOG_REC_CLUST_DELETE_MARK_8027 = 10,
+
+    /// Mark secondary index record deleted
+    MLOG_REC_SEC_DELETE_MARK = 11,
+
+    /// update of a record, preserves record field sizes
+    MLOG_REC_UPDATE_IN_PLACE_8027 = 13,
+
+    /// Delete a record from a page
+    MLOG_REC_DELETE_8027 = 14,
+
+    /// Delete record list end on index page
+    MLOG_LIST_END_DELETE_8027 = 15,
+
+    /// Delete record list start on index page
+    MLOG_LIST_START_DELETE_8027 = 16,
+
+    /// Copy record list end to a new created index page
+    MLOG_LIST_END_COPY_CREATED_8027 = 17,
+
+    /// Reorganize an index page in ROW_FORMAT=REDUNDANT
+    MLOG_PAGE_REORGANIZE_8027 = 18,
+
+    /// Create an index page
+    MLOG_PAGE_CREATE = 19,
+
+    /// Insert entry in an undo log
+    MLOG_UNDO_INSERT = 20,
+
+    /// erase an undo log page end
+    MLOG_UNDO_ERASE_END = 21,
+
+    /// initialize a page in an undo log
+    MLOG_UNDO_INIT = 22,
+
+    /// reuse an insert undo log header
+    MLOG_UNDO_HDR_REUSE = 24,
+
+    /// create an undo log header
+    MLOG_UNDO_HDR_CREATE = 25,
+
+    /// mark an index record as the predefined minimum record
+    MLOG_REC_MIN_MARK = 26,
+
+    /// initialize an ibuf bitmap page
+    MLOG_IBUF_BITMAP_INIT = 27,
+
+    /// Current LSN
+    MLOG_LSN = 28,
+
+    /// this means that a file page is taken into use and the prior contents of
+    /// the page should be ignored: in recovery we must not trust the lsn values
+    /// stored to the file page. Note: it's deprecated because it causes crash
+    /// recovery problem in bulk create index, and actually we don't need to reset
+    /// page lsn in recv_recover_page_func() now.
+    MLOG_INIT_FILE_PAGE = 29,
+
+    /// write a string to a page
+    MLOG_WRITE_STRING = 30,
+
+    /// If a single mtr writes several log records, this log record ends the
+    /// sequence of these records
+    MLOG_MULTI_REC_END = 31,
+
+    /// dummy log record used to pad a log block full
+    MLOG_DUMMY_RECORD = 32,
+
+    /// log record about creating an .ibd file, with format
+    MLOG_FILE_CREATE = 33,
+
+    /// rename a tablespace file that starts with (space_id,page_no)
+    MLOG_FILE_RENAME = 34,
+
+    /// delete a tablespace file that starts with (space_id,page_no)
+    MLOG_FILE_DELETE = 35,
+
+    /// mark a compact index record as the predefined minimum record
+    MLOG_COMP_REC_MIN_MARK = 36,
+
+    /// create a compact index page
+    MLOG_COMP_PAGE_CREATE = 37,
+
+    /// compact record insert
+    MLOG_COMP_REC_INSERT_8027 = 38,
+
+    /// mark compact clustered index record deleted
+    MLOG_COMP_REC_CLUST_DELETE_MARK_8027 = 39,
+
+    /// mark compact secondary index record deleted; this log record type is
+    /// redundant, as MLOG_REC_SEC_DELETE_MARK is independent of the record
+    /// format.
+    MLOG_COMP_REC_SEC_DELETE_MARK = 40,
+
+    /// update of a compact record, preserves record field sizes
+    MLOG_COMP_REC_UPDATE_IN_PLACE_8027 = 41,
+
+    /// delete a compact record from a page
+    MLOG_COMP_REC_DELETE_8027 = 42,
+
+    /// delete compact record list end on index page
+    MLOG_COMP_LIST_END_DELETE_8027 = 43,
+
+    /// * delete compact record list start on index page
+    MLOG_COMP_LIST_START_DELETE_8027 = 44,
+
+    /// copy compact record list end to a new created index page
+    MLOG_COMP_LIST_END_COPY_CREATED_8027 = 45,
+
+    /// reorganize an index page
+    MLOG_COMP_PAGE_REORGANIZE_8027 = 46,
+
+    /// write the node pointer of a record on a compressed non-leaf B-tree page
+    MLOG_ZIP_WRITE_NODE_PTR = 48,
+
+    /// write the BLOB pointer of an externally stored column on a compressed page
+    MLOG_ZIP_WRITE_BLOB_PTR = 49,
+
+    /// write to compressed page header
+    MLOG_ZIP_WRITE_HEADER = 50,
+
+    /// compress an index page
+    MLOG_ZIP_PAGE_COMPRESS = 51,
+
+    /// compress an index page without logging it's image
+    MLOG_ZIP_PAGE_COMPRESS_NO_DATA_8027 = 52,
+
+    /// reorganize a compressed page
+    MLOG_ZIP_PAGE_REORGANIZE_8027 = 53,
+
+    /// Create a R-Tree index page
+    MLOG_PAGE_CREATE_RTREE = 57,
+
+    /// create a R-tree compact page
+    MLOG_COMP_PAGE_CREATE_RTREE = 58,
+
+    /// this means that a file page is taken into use. We use it to replace
+    /// MLOG_INIT_FILE_PAGE.
+    MLOG_INIT_FILE_PAGE2 = 59,
+
+    /// Table is being truncated. (Marked only for file-per-table), Disabled for
+    /// WL6378
+    MLOG_TRUNCATE = 60,
+
+    /// notify that an index tree is being loaded without writing redo log about
+    /// individual pages
+    MLOG_INDEX_LOAD = 61,
+
+    /// log for some persistent dynamic metadata change
+    MLOG_TABLE_DYNAMIC_META = 62,
+
+    /// create a SDI index page
+    MLOG_PAGE_CREATE_SDI = 63,
+
+    /// create a SDI compact page
+    MLOG_COMP_PAGE_CREATE_SDI = 64,
+
+    /// Extend the space
+    MLOG_FILE_EXTEND = 65,
+
+    /// Used in tests of redo log. It must never be used outside unit tests.
+    MLOG_TEST = 66,
+
+    MLOG_REC_INSERT = 67,
+    MLOG_REC_CLUST_DELETE_MARK = 68,
+    MLOG_REC_DELETE = 69,
+    MLOG_REC_UPDATE_IN_PLACE = 70,
+    MLOG_LIST_END_COPY_CREATED = 71,
+    MLOG_PAGE_REORGANIZE = 72,
+    MLOG_ZIP_PAGE_REORGANIZE = 73,
+    MLOG_ZIP_PAGE_COMPRESS_NO_DATA = 74,
+    MLOG_LIST_END_DELETE = 75,
+    MLOG_LIST_START_DELETE = 76,
+
+    /// biggest value (used in assertions)
+    // MLOG_BIGGEST_TYPE = MLOG_LIST_START_DELETE
+
+    #[default]
+    UNDEF,
+}
+
+/// log record header
+#[derive(Clone, Derivative)]
+#[derivative(Debug)]
+pub struct LogRecordHeader {
+    /// block address
+    #[derivative(Debug(format_with = "util::fmt_addr"))]
+    pub addr: usize,
+
+    /// block data buffer
+    #[derivative(Debug = "ignore")]
+    pub buf: Arc<Bytes>,
+
+    /// log record type
+    pub log_rec_type: LogRecordTypes,
+
+    /// single record flag, see MLOG_SINGLE_REC_FLAG
+    pub single_rec_flag: bool,
+}
+
+impl LogRecordHeader {
+    /// if the mtr contains only one log record for one page, i.e.,
+    /// write_initial_log_record has been called only once, this flag is ORed to
+    /// the type of that first log record
+    const MLOG_SINGLE_REC_FLAG: u8 = 0x80;
+
+    pub fn new(addr: usize, buf: Arc<Bytes>) -> Self {
+        let b0 = util::u8_val(&buf, addr);
+        Self {
+            log_rec_type: (b0 & !Self::MLOG_SINGLE_REC_FLAG).into(),
+            single_rec_flag: (b0 & Self::MLOG_SINGLE_REC_FLAG) > 0,
             buf: buf.clone(),
             addr,
         }
