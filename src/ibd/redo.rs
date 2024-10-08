b8afbae0216c56664a30045883857e5b34a6de62
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-10-08 16:15:02 +0800

    doc

--- src/ibd/redo.rs
+++ src/ibd/redo.rs
@@ -13,10 +13,15 @@ use crate::{
     util,
 };
 
-// log file size
+/// The next value should be smaller or equal to the smallest sector size used
+/// on any disk. A log block is required to be a portion of disk which is
+/// written so that if the start and the end of a block get written to disk,
+/// then the whole block gets written. This should be true even in most cases of
+/// a crash: if this fails for a log block, then it is equivalent to a media
+/// failure in the log.
 pub const OS_FILE_LOG_BLOCK_SIZE: usize = 512;
 
-// Maximum allowed block's number (stored in hdr_no) increased by 1.
+/// Maximum allowed block's number (stored in hdr_no) increased by 1.
 pub const LOG_BLOCK_MAX_NO: usize = 0x3FFFFFFF + 1;
 
 pub fn log_block_guess_lsn(hdr_no: u32, epoch_no: u32) -> u64 {
